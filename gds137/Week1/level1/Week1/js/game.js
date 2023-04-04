f7d6f0508af08b53fbb09da5b36d4a9c4b4b5014
rft0202 <rftester@email.neit.edu>
2023-04-04 18:19:00 -0400

    increased speed upon collision

--- gds137/Week1/level1/Week1/js/game.js
+++ gds137/Week1/level1/Week1/js/game.js
@@ -3,7 +3,7 @@
 var canvas;
 var context;
 //v 1000 ms or 1 second / FPS
-var interval = 1000/60;
+var interval = 1000/55;
 
 	//Set Up the Canvas
 	canvas = document.getElementById("canvas");
@@ -29,23 +29,55 @@ function animate()
 	if(ball.x < ball.width/2)
 	{
 		ball.x = ball.width/2;
-		ball.vx = -ball.vx;
+		if(ball.vx > 0 && ball.vx < 55){
+			ball.vx = -ball.vx - 1;
+		}
+		else if (ball.vx < 0 && ball.vx > -55){
+			ball.vx = -ball.vx + 1;
+		}
+		else{
+			ball.vx = -ball.vx; //stops the ball from going too fast
+		}
 	}
 	if(ball.x > canvas.width - ball.width/2)
 	{
 		ball.x = canvas.width - ball.width/2;
-		ball.vx = -ball.vx;
+		if(ball.vx > 0 && ball.vx < 55){
+			ball.vx = -ball.vx - 1;
+		}
+		else if (ball.vx < 0 && ball.vx > -55){
+			ball.vx = -ball.vx + 1;
+		}
+		else{
+			ball.vx = -ball.vx; 
+		}
 	}
 
 	if(ball.y < ball.height/2)
 	{
 		ball.y = ball.height/2;
-		ball.vy = -ball.vy;
+		if(ball.vy > 0 && ball.vy < 55){
+			ball.vy = -ball.vy - 1;
+		}
+		else if (ball.vx < 0 && ball.vx > -55){
+			ball.vy = -ball.vy + 1;
+		}
+		else{
+			ball.vy = -ball.vy;
+		}
 	}
 	if(ball.y > canvas.height - ball.height/2)
 	{
 		ball.y = canvas.height - ball.height/2;
-		ball.vy = -ball.vy;
+		if(ball.vy > 0 && ball.vy < 55){
+			ball.vy = -ball.vy - 1;
+		}
+		else if (ball.vx < 0 && ball.vx > -55){
+			ball.vy = -ball.vy + 1;
+		}
+		else{
+			ball.vy = -ball.vy;
+		}
 	}
 
 	//Update the Screen (redraw the elements)
