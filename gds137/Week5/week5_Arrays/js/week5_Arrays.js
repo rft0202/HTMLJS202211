2a459e6ed8ca3176f59b029fe3c80a6fb19b5c51
rft0202 <rftester@email.neit.edu>
2023-04-27 13:15:35 -0400

    week 5 in class

--- gds137/Week5/week5_Arrays/js/week5_Arrays.js
+++ gds137/Week5/week5_Arrays/js/week5_Arrays.js
@@ -23,24 +23,49 @@ var dots = [];
 for(var i = 0; i < amt; i++)
 {
 	dots[i] = new GameObject();
-	dots[i].x = Math.random() * canvas.width;
-	dots[i].y = Math.random() * canvas.height;
+	dots[i].x = Math.random() * canvas.width; //0-canvas.width
+	dots[i].y = Math.random() * canvas.height; //0-canvas.height
 	dots[i].width = rand(5,20);
 	dots[i].vy = rand(-40,40);
 	dots[i].vx = rand(-10,10);
-	dots[i].color = colors[Math.floor(rand(0,2.9))];
+	//dots[i].color = colors[Math.floor(rand(0,2.9))];
+	dots[i].color = `rgb(${rand(0,255)},${rand(0,255)}, ${rand(0,255)})`; //random color
 }
+
+//added v
+var states = []
+var currentState = 'square';
+//var currentState = 'circle';
+
+//state machine:
+states['square'] = function(){dots[0].drawRect()}
+states['circle'] = function(){dots[0].drawCircle()}
+//added ^
 
 function animate()
 {
 	context.clearRect(0,0,canvas.width, canvas.height);	
-	
+
+	states[currentState](); //added
+	/*
+	//The for loops should be used for platforms, enemies, items, etc.
 	for(var i = 0; i < amt; i++)
 	{
 		dots[i].drawCircle();
-		//dots[i].vy += gravity;
-		//dots[i].move();
+		//dots[i].vy *= .87;
+		dots[i].vy += gravity; //gravity
+
+		dots[i].move();
+		//added v
+		if(dots[i].y > canvas.height - dots[i].height/2)
+		{
+			dots[i].y = canvas.height - dots[i].height/2;
+			dots[i].vy = -dots[i].vy * .87;
+		}
+		//added ^
+		dots[i].drawCircle();
 	}
+	*/
 }
 
 
